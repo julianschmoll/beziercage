aef92d1a69870b6171ea5d6dfc8d86267ddd199b
julianschmoll <github@julianschmoll.de>
2024-10-28 10:43:28 +0100

    update MTypeId

--- src/jSmearDeformer.cpp
+++ src/jSmearDeformer.cpp
@@ -1,7 +1,10 @@
 #include "jSmearDeformer.h"
 #include <maya/MItGeometry.h>
 
-MTypeId jSmear::id(0x0011580B);
+/* According to https://mayaid.autodesk.io/ the numeric range 0 - 0x7ffff
+(524288 ids) is reserved for plugins that will be internal. Since
+registration is mostly for companies, this is a random number in this range. */
+MTypeId jSmear::id(0x6ccc);
 
 const char* jSmear::kName = "jSmear";
 
