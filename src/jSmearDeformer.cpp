d4a704499a6d50de9c5f741f8d757d97be9f1f73
js435 <dev@julianschmoll.de>
2024-11-12 12:48:21 +0100

    add deformer attributes and make deformer paintable

--- src/jSmearDeformer.cpp
+++ src/jSmearDeformer.cpp
@@ -1,15 +1,73 @@
-#include "jSmearDeformer.h"
-#include <maya/MItGeometry.h>
-#include <maya/MFnCompoundAttribute.h>
+# include "jSmearDeformer.h"
+
 
 // This ID is registered with Autodesk and should not clash with other nodes.
 MTypeId jSmear::id(0x13f8c0);
+MObject jSmear::aTime;
+MObject jSmear::aNormalOffset;
+MObject jSmear::aAngleMagnitude;
+MObject jSmear::aStartFrame;
+MObject jSmear::aSmearFrames;
+MObject jSmear::aMinSmearVelocity;
+MObject jSmear::aMaxSmearVelocity;
+MObject jSmear::aWorldMatrix;
 
 const char* jSmear::kName = "jSmear";
 // declare static attributes (MObject jSmear::aDriverGeo; ...)
 
 
 MStatus jSmear::initialize() {
+    MFnMatrixAttribute      mAttr;
+    MFnNumericAttribute     nAttr;
+    MFnUnitAttribute        uAttr;
+    MStatus				    status;
+
+    aTime = uAttr.create("time", "time", MFnUnitAttribute::kTime, 0.0);
+    addAttribute(aTime);
+    attributeAffects(aTime, outputGeom);
+
+    aWorldMatrix = mAttr.create("worldMatrix", "worldMatrix");
+    addAttribute(aWorldMatrix);
+    attributeAffects(aWorldMatrix, outputGeom);
+
+    aStartFrame = nAttr.create("startFrame", "startFrame", MFnNumericData::kInt, 0, &status);
+    nAttr.setKeyable(true);
+    addAttribute(aStartFrame);
+
+    aSmearFrames = nAttr.create("smearFrames", "smearFrames", MFnNumericData::kInt, 1, &status);
+    nAttr.setMin(1);
+    nAttr.setMax(100);
+    nAttr.setKeyable(true);
+    addAttribute(aSmearFrames);
+    attributeAffects(aSmearFrames, outputGeom);
+
+    aNormalOffset = nAttr.create("normalOffset", "normalOffset", MFnNumericData::kFloat, 0.0, &status);
+    nAttr.setMin(0.0);
+    nAttr.setKeyable(true);
+    addAttribute(aNormalOffset);
+    attributeAffects(aNormalOffset, outputGeom);
+
+    aAngleMagnitude = nAttr.create("angleMagnitude", "angleMagnitude", MFnNumericData::kFloat, 1.0, &status);
+    nAttr.setMin(0.0);
+    nAttr.setKeyable(true);
+    addAttribute(aAngleMagnitude);
+    attributeAffects(aAngleMagnitude, outputGeom);
+
+    aMinSmearVelocity = nAttr.create("minSmearVelocity", "minSmearVelocity", MFnNumericData::kDouble, 0, &status);
+    nAttr.setMin(0.0);
+    nAttr.setKeyable(true);
+    addAttribute(aMinSmearVelocity);
+    attributeAffects(aMinSmearVelocity, outputGeom);
+
+    aMaxSmearVelocity = nAttr.create("maxSmearVelocity", "maxSmearVelocity", MFnNumericData::kDouble, 5, &status);
+    nAttr.setMin(0.0);
+    nAttr.setKeyable(true);
+    addAttribute(aMaxSmearVelocity);
+    attributeAffects(aMaxSmearVelocity, outputGeom);
+
+    // Makes the deformer weight paintable
+    MGlobal::executeCommand("makePaintable -attrType multiFloat -sm deformer jSmear weights");
+
     return MS::kSuccess;
 }
 
@@ -28,5 +86,5 @@ void* jSmear::creator() {
 
 MStatus jSmear::deform(MDataBlock& data, MItGeometry& itGeo, const MMatrix& localToWorldMatrix, unsigned int geomIndex) {
     MStatus status;
-    return MS::kSuccess;
+    return status;
 }
--- src/jSmearDeformer.h
+++ src/jSmearDeformer.h
@@ -1,7 +1,15 @@
 #ifndef JSMEARDEFORMER_H
 #define JSMEARDEFORMER_H
 
+
 #include <maya/MPxDeformerNode.h>
+#include <maya/MItGeometry.h>
+#include <maya/MFnCompoundAttribute.h>
+#include <maya/MFnMatrixAttribute.h>
+#include <maya/MFnNumericAttribute.h>
+#include <maya/MFnTypedAttribute.h>
+#include <maya/MFnUnitAttribute.h>
+#include <maya/MGlobal.h>
 
 
 class jSmear : public MPxDeformerNode {
@@ -13,8 +21,10 @@ class jSmear : public MPxDeformerNode {
         virtual MStatus redoIt();
         virtual bool isUndoable() const;
         static void* creator();
+        static  MStatus initialize();
         static MSyntax newSyntax();
-
+        virtual MStatus deform(MDataBlock& data, MItGeometry& iter, const MMatrix& mat,
+                           unsigned int mIndex);
         const static char* kName;
 
         static MObject aDriverGeo; //driven geometry for deformer
@@ -22,6 +32,14 @@ class jSmear : public MPxDeformerNode {
         static MObject aSampleComponents; //Vertex IDs
         static MObject aSampleWeights; // Vertex weights
         static MObject aBindMatrix;
-
+        static MTypeId id;
+        static MObject aTime;
+        static MObject aStartFrame;
+        static MObject aMinSmearVelocity;
+        static MObject aMaxSmearVelocity;
+        static MObject aWorldMatrix;
+        static MObject aSmearFrames;
+        static MObject aNormalOffset;
+        static MObject aAngleMagnitude;
 };
 #endif
