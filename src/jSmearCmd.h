d26bd49b0aa8458e0b989e9930d4a3a02d6b820d
js435 <dev@julianschmoll.de>
2024-11-20 11:14:06 +0100

    fix includes

--- src/jSmearCmd.h
+++ src/jSmearCmd.h
@@ -1,20 +1,44 @@
 #ifndef JSMEARCMD_H
 #define JSMEARCMD_H
 
+#include <maya/MSelectionList.h>
+#include <maya/MDagPath.h>
+#include <maya/MDagPathArray.h>
+#include <maya/MPlug.h>
+#include <maya/MString.h>
+#include <maya/MStringArray.h>
+#include <maya/MGlobal.h>
+#include <maya/MSyntax.h>
+#include <maya/MString.h>
 #include <maya/MArgList.h>
-#include <maya/MPxCommand.h>
+#include <maya/MArgDatabase.h>
+#include <maya/MObject.h>
+#include <maya/MDGModifier.h>
 
+#include <maya/MItGeometry.h>
+#include <maya/MItDependencyGraph.h>
+
+#include <maya/MFnDagNode.h>
+#include <maya/MFnDependencyNode.h>
+#include <maya/MFn.h>
+#include <maya/MFnData.h>
+#include <maya/MFnMesh.h>
+#include <maya/MFnTransform.h>
+
+#include <maya/MPxCommand.h>
 
 class jSmearCmd : public MPxCommand {
     public:
         jSmearCmd();
-        virtual ~jSmear();
-        virtual MStatus deform(MDataBlock& data, MItGeometry& iter, const MMatrix& mat, unsigned int mIndex);
         static void* creator();
         static MStatus initialize();
+        virtual MStatus	doIt(const MArgList&);
+        virtual MStatus undoIt();
+        virtual MStatus redoIt();
+        virtual bool isUndoable() const;
 
         const static char* kName;
         static MTypeId id;
-
+        static MSyntax newSyntax();
 };
 #endif
