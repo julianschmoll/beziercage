f7a14102a8328a452465195cf746e0d3a26bf63d
js435 <dev@julianschmoll.de>
2024-11-20 11:30:29 +0100

    delete all build files except relevant ones

--- build.bat
+++ build.bat
@@ -6,4 +6,21 @@ mkdir %builddir%
 cd %builddir%
 cmake -A x64 -T v143 -DMAYA_VERSION=%1 ../
 cmake --build . --target install --config Release
+
+set "target_folder=%cd%"
+
+for %%F in ("%target_folder%\*") do (
+    if /I not "%%~nxF"=="jSmear" (
+        echo Deleting file %%F
+        del /q "%%F"
+    )
+)
+
+for /d %%D in ("%target_folder%\*") do (
+    if /I not "%%~nxD"=="jSmear" (
+        echo Deleting directory %%D
+        rmdir /s /q "%%D"
+    )
+)
+
 cd ..
