d531ae6e86be7621608856d65c2b318a72d76c5a
julianschmoll <github@julianschmoll.de>
2024-12-02 15:56:49 +0100

    add common

---
+++ src/common.cpp
@@ -0,0 +1,22 @@
+# include <common.h>
+
+bool IsShapeNode(MDagPath &path)
+{
+    // ToDo: Implement
+    return false;
+}
+
+MStatus GetShapeNode(MDagPath &path, bool intermediate)
+{
+    return MStatus();
+}
+
+MStatus GetDagPath(MString &name, MDagPath &path)
+{
+    return MStatus();
+}
+
+MStatus DeleteIntermediateObjects(MDagPath &path)
+{
+    return MStatus();
+}
---
+++ src/common.h
@@ -0,0 +1,52 @@
+/**
+  Contains various helper functions.
+*/
+
+#ifndef JSMEAR_COMMON_H
+#define JSMEAR_COMMON_H
+
+#include <maya/MDagPath.h>
+#include <maya/MDoubleArray.h>
+#include <maya/MFloatVectorArray.h>
+#include <maya/MIntArray.h>
+#include <maya/MMatrix.h>
+#include <maya/MPoint.h>
+#include <maya/MPointArray.h>
+#include <maya/MString.h>
+#include <map>
+#include <vector>
+#include <set>
+
+
+/**
+  Checks if provided path is for a shape node.
+  @param[in] path the dag path.
+  @return true if provided path is for a shape node.
+ */
+bool IsShapeNode(MDagPath& path);
+
+
+/**
+  Sets dag path to shape node path from transform.
+  @param[in,out] path Path to a dag node.
+  @param[in] intermediate setting to get intermediate nodes.
+  @return MStatus.
+ */
+MStatus GetShapeNode(MDagPath& path, bool intermediate=false);
+
+
+/**
+  Gets Dag Path of an object by name.
+  @param[in] name Name of a dag node.
+  @param[out] path Storage for the dag path.
+ */
+MStatus GetDagPath(MString& name, MDagPath& path);
+
+
+/**
+  Delete intermediate shape nodes of the given dag path.
+  @param[in] path DagPath.
+ */
+MStatus DeleteIntermediateObjects(MDagPath& path);
+
+#endif
