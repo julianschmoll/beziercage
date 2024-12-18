1acf2ffdb8c865d850444a7ebfa5a7d077b30ff4
Julian Schmoll <soi@julianschmoll.de>
2024-12-18 15:37:54 +0100

    check unloading as well

--- tests/test_jsmear_init.py
+++ tests/test_jsmear_init.py
@@ -4,9 +4,19 @@ import unittest
 from maya import cmds
 
 
+PLUGIN = "jSmear.mll"
+
 class TestJsmearInit(unittest.TestCase):
     """Test Initialization of jSmear."""
 
     def test_init(self):
         """Test Initialization of jSmear."""
-        cmds.loadPlugin("jSmear.mll")
+        cmds.loadPlugin(PLUGIN)
+        _check_plugin_state(loaded=True)
+        cmds.unloadPlugin(PLUGIN)
+        _check_plugin_state(loaded=False)
+
+
+def _check_plugin_state(loaded=True):
+    plugin_loaded = cmds.pluginInfo(PLUGIN, query=True, loaded=True)
+    assert loaded == plugin_loaded
