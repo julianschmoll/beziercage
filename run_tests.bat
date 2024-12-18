c5b8ee8299f70ad57b3ece1c320ef7b3a3d2f6ae
julianschmoll <github@julianschmoll.de>
2024-12-18 12:02:07 +0100

    don't pause

--- run_tests.bat
+++ run_tests.bat
@@ -5,11 +5,7 @@ set thisDir=%~dp0
 set testfile="%thisDir%%packageName%\tests\run_tests.py"
 set mayapy="C:\Program Files\Autodesk\%maya%\bin\mayapy.exe"
 
-echo Running tests in batch mode with maya.standalone (%maya%).
-echo This dir: %thisDir%
-echo Running in mayapy: %mayapy%
-echo Executing python file: %testfile%
+echo Running tests for Maya %1.
 echo ----------------------------------------------------------
 
 %mayapy% %testfile% %thisDir% %1
-pause
