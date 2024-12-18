dd7bd702578116f4357b34a5ddedc5d3a4f83d9f
Julian Schmoll <soi@julianschmoll.de>
2024-12-18 15:37:12 +0100

    disable execution of userSetups and warning prompt when loading plugin

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -4,14 +4,19 @@ from pathlib import Path
 import sys
 import unittest
 
+from maya import cmds
 import maya.standalone
+import maya.app.commands
+
+
+orig_process_command_list = maya.app.commands.processCommandList
 
 
 def run_batch_tests():
     """Runs all configured tests."""
     _setup_environment()
     test_path = Path(__file__).parent
-    suite = unittest.TestLoader().discover(test_path)
+    suite = unittest.TestLoader().discover(str(test_path))
     runner = unittest.TextTestRunner(
         stream=sys.stdout,
         descriptions=True,
@@ -26,8 +31,18 @@ def _setup_environment():
     sys.path.append(path)
     root_path = Path(__file__).parent.parent
     plug_in_path = Path(root_path / "build" / version / "jSmear" / "plug-ins")
+    if not plug_in_path.exists():
+        print("Please build Plugin before testing!")
+        sys.exit(1)
     os.environ["MAYA_PLUG_IN_PATH"] = str(plug_in_path)
+    maya.app.commands.processCommandList = _set_option_vars
     maya.standalone.initialize(name="python")
+
+
+def _set_option_vars():
+    orig_process_command_list()
+    cmds.optionVar(iv=("SafeModeExecUserSetupScript", 0))
+    cmds.optionVar(iv=("TrustCenterPathOption", 0))
 
 
 if __name__ == "__main__":
