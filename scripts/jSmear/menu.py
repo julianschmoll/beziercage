6ce09aa3001b5b32702faf5932529d04e3e8396f
julianschmoll <github@julianschmoll.de>
2024-12-19 15:28:20 +0100

    use Node ID to identify menu items

--- scripts/jSmear/menu.py
+++ scripts/jSmear/menu.py
@@ -3,46 +3,54 @@ from maya import cmds
 from maya import mel
 
 
-MENU_ITEMS = []
-INSERT_AFTER = "Morph"
+ID = 0x0011580B
 
 
 def create():
     """Creates jSmear Maya Menu."""
-    global MENU_ITEMS
-    section = None
-
-    if MENU_ITEMS:
-        return
+    destroy()
     for menu in ['mainDeformMenu', 'mainRigDeformationsMenu']:
         mel.eval(f'ChaDeformationsMenu MayaWindow|{menu};')
         items = cmds.menu(menu, q=True, ia=True)
+        section = None
         for item in items:
             if cmds.menuItem(item, q=True, divider=True):
                 section = cmds.menuItem(item, q=True, label=True)
             menu_label = cmds.menuItem(item, q=True, label=True)
-            if menu_label == INSERT_AFTER and section == 'Create':
+            if menu_label == "Morph" and section == "Create":
                 jsmear = cmds.menuItem(
-                    label="jSmear", command=_create_deformer, sourceType='python', insertAfter=item, parent=menu
+                    label="jSmear",
+                    command=_create_deformer,
+                    insertAfter=item,
+                    parent=menu,
+                    annotation="Creates a jSmear Deformer on Current Selection with default options.",
+                    data=ID
                 )
-                jsmear_options = cmds.menuItem(
-                    command=_display_options, insertAfter=cvwrap_item, parent=menu, optionBox=True
+                cmds.menuItem(
+                    label="jSmearOptions",
+                    command=_display_options,
+                    insertAfter=jsmear,
+                    parent=menu,
+                    optionBox=True,
+                    annotation="Opens Optioins Dialog for jSmear Deformer.",
+                    data=ID
                 )
-                MENU_ITEMS.append(jsmear)
-                MENU_ITEMS.append(jsmear_options)
 
 
 def destroy():
     """Destroys jSmear Maya Menu"""
-    global MENU_ITEMS
-    for item in MENU_ITEMS:
-        cmds.deleteUI(item, menuItem=True)
-    MENU_ITEMS = []
+    for menu in ['mainDeformMenu', 'mainRigDeformationsMenu']:
+        mel.eval('ChaDeformationsMenu MayaWindow|{0};'.format(menu))
+        items = cmds.menu(menu, q=True, ia=True)
+        for item in items:
+            data =  cmds.menuItem(item, q=True, data=True)
+            if data == ID:
+                cmds.deleteUI(item, menuItem=True)
 
 
-def _create_deformer():
+def _create_deformer(*args, **kwargs):
     pass
 
 
-def _display_options():
+def _display_options(*args, **kwargs):
     pass
