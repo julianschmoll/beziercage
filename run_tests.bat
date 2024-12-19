5b3a2ba7e8ef1d698fa5a860a0854b22d8b8ee30
julianschmoll <github@julianschmoll.de>
2024-12-19 12:16:13 +0100

    match line lengths in print

--- run_tests.bat
+++ run_tests.bat
@@ -6,6 +6,6 @@ set testfile="%thisDir%%packageName%\tests\run_tests.py"
 set mayapy="C:\Program Files\Autodesk\%maya%\bin\mayapy.exe"
 
 echo Running tests for Maya %1.
-echo ----------------------------------------------------------
+echo ----------------------------------------------------------------------
 
 %mayapy% %testfile% %thisDir% %1
